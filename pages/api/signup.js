f1fdbb317fac9a7f805d9787d20125726bd156f7
Francis O'Brien <gilgamesh@gmail.com>
2021-02-24 22:07:28 +0000

    add basic signup function

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,31 +1,42 @@
 var beanDB = require('faunadb')
+var crypto = require('crypto')
 
 const dbClient = new beanDB.Client({ secret: process.env.FKEY })
 const q = beanDB.query
-//todo - write function to format data for fauna query
-const d = new Date();
-const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
-const mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(d);
-const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
-const today = new String(`${ye}-${mo}-${da}`).toString()
 
 export default async (req, res) => {
-  var data = null
-  try {
-    data = await dbClient.query(
-      q.Get(
-        q.Match(q.Index('beanDate'), today)
-      )
-    )
-  } catch (e) {
-    console.log(e)
-    res.status(500).json('Something Went Wrong!')
-    return
-  }
-  console.log(data)
-  if (data) {
-    res.status(200).json(data.data)
-  } else {
-    res.status(500).json({ Error: 'No Bean Found'})
-  }
+    const body = req.body
+    try {
+            dbClient.query(
+            q.Get(
+                q.Match(q.Index('emailused'), body.email)
+            )
+        ).then(result => {
+            res.status(400).json({ error: 'User Already Exists' })
+            return
+        })
+    } catch (e) {
+        console.log(e)
+    }
+
+    var userCreated = null;
+
+    try {
+        userCreated = await dbClient.query(
+            q.Create(
+                q.Collection('users'),
+                {
+                    data: body   
+                }
+            )
+        )
+    } catch {
+        userCreated = false
+    }
+    if (!userCreated) {
+        res.status(500).json({ error: 'Unable to Create User' })
+        return
+    } else {
+        res.status(200).json({ message: 'User Created Successfully'})
+    }
 }
